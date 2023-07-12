b838ddcc7afc4b180a615acee74b37ccac0f1b52
ViniciiusCampos <134455972+ViniiciiusSantos@users.noreply.github.com>
2023-07-12 11:40:49 -0300

    slider no modal adicionado.

--- script/script.js
+++ script/script.js
@@ -12,7 +12,7 @@ const compra = document.querySelector('.compra');
 const lixo = document.querySelector('.lixo');
 const fechar = document.querySelector('.close');
 const modalJanela = document.querySelector('.modal-janela');
-const imgContainer = document.querySelector('#img-abrirmodal');
+const imgContainer = document.querySelector('#img-modal');
 
 let adicional = 0;
 
@@ -46,27 +46,67 @@ function miniMenu() {
     asideIn.classList.toggle('aparecer');
 }
 
-window.addEventListener('load', function() {
-    var miniImgs = document.querySelectorAll('.mini-img img');
-    var imgPrincipal = document.querySelector('.img-principal');
-    miniImgs.forEach(function(miniImg) {
+var miniImgs = document.querySelectorAll('.mini-img img');
+var imgPrincipal = document.querySelector('.img-principal');
+miniImgs.forEach(function(miniImg) {
     miniImg.addEventListener('click', function() {
         var srcMaior = this.getAttribute('data-src-maior');
         imgPrincipal.src = srcMaior;
         miniImgs.forEach(function(img) {
-        img.classList.remove('selecionada');
+            img.classList.remove('selecionada');
         });
         this.classList.add('selecionada');
-    });
     });
 });
 
 imgContainer.addEventListener('click', () => {
-   modalJanela.style.display = 'flex'
-})
+    modalJanela.style.display = 'flex';
+});
 
 fechar.addEventListener('click', () => {
     modalJanela.style.display = 'none';
-})
+});
+
+var miniImgsModal = document.querySelectorAll('#mini-img img');
+var imgPrincipalModal = document.querySelector('#img-principal');
+var indiceAtual = 0;
+
+function handleMiniImgClickModal() {
+    var srcMaior = this.getAttribute('data-new-src');
+    imgPrincipalModal.src = srcMaior;
+
+    miniImgsModal.forEach(function(img, index) {
+        img.classList.remove('selecio');
+        if(index === indiceAtual) {
+            img.classList.add('selecio');
+        }
+    });
+}
+function exibirImagem(index) {
+    if (index >= 0 && index < miniImgsModal.length) {
+        var miniImg = miniImgsModal[index];
+        var srcMaior = miniImg.getAttribute('data-new-src');
+        imgPrincipalModal.src = srcMaior;
+        miniImgsModal.forEach(function(img) {
+            img.classList.remove('selecio');
+        });
+        miniImg.classList.add('selecio');
+        indiceAtual = index;
+    }
+}
+
+document.getElementById('btn-anterior').addEventListener('click', function() {
+    exibirImagem(indiceAtual - 1);
+});
+
+document.getElementById('btn-proximo').addEventListener('click', function() {
+    exibirImagem(indiceAtual + 1);
+});
+
+miniImgsModal.forEach(function(miniImg, index) {
+    miniImg.addEventListener('click', function() {
+        exibirImagem(index);
+    });
+});
 
 cartLogo.addEventListener('click', miniMenu)
